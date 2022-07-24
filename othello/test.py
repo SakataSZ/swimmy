bc1db1c0dd05c3438c9382133d52b57d2d20f0bd
SakataSZ <sakamoto.yuta.sz@gmail.com>
2022-07-24 20:26:58 +0900

    update othello.md add comment othello.py

--- othello/test.py
+++ othello/test.py
@@ -1,9 +1,12 @@
-SPACE = 0   # 何も置かれていない
-BLACK = 1   # 黒
-WHITE = 2   # 白
-OUT = -1    # 盤面の外(番兵)
-SIZE = 10   # 盤面の大きさ
+SPACE = 0  # 何も置かれていない
+BLACK = 1  # 黒
+WHITE = 2  # 白
+OUT = -1  # 盤面の外(番兵)
+SIZE = 10  # 盤面の大きさ
+
+# プレイヤーを表す数値を文字列に変換する為の辞書
 PLAYER = {BLACK: "黒", WHITE: "白"}
+BOARD = {BLACK: "●", WHITE: "○", SPACE: "□"}
 
 
 def show_array(board):
@@ -43,20 +46,16 @@ def show_board(board):
 
 
 def get_move(player):
-    # 正しい形式で入力されるまで繰り返す
     while True:
         print(PLAYER[player] + "の番です")
         move = input("指し手を入力してください : ")
         move = move.split(' ')
-        # 2つの文字がスペース区切りで入力されているか
         if len(move) == 2:
             i = move[0]
             j = move[1]
-            # 2つの文字が数字か
             if i.isdecimal() and j.isdecimal():
-                i = int(i)      # 数値に変換
-                j = int(j)      # 数値に変換
-                # 2つの数値が盤面に収まる値か
+                i = int(i)
+                j = int(j)
                 if 1 <= i <= 8 and 1 <= j <= 8:
                     return [i, j]
         print("正しい形式で入力して下さい\n")
@@ -78,9 +77,9 @@ def check(board, player, j):
 
 def bi_check(board, player, j, dj):
     look = 1
-    while board[j + dj*look] == opp(player):
+    while board[j + dj * look] == opp(player):
         look += 1
-    if board[j + dj*look] == player:
+    if board[j + dj * look] == player:
         return look - 1
     else:
         return 0
@@ -125,8 +124,44 @@ def set_and_turn_over(board, player, i, j):
                 continue
             count = count_turn_over(board, player, i, j, di, dj)
             for c in range(count + 1):
-                board[i + c*di][j + c*dj] = player
+                board[i + c * di][j + c * dj] = player
+
     board[i][j] = player
+
+
+def exit_legal_move(board, player):
+    for i in range(1, SIZE - 1):
+        for j in range(1, SIZE - 1):
+            if is_legal_move(board, player, i, j):
+                return True
+
+    return False
+
+
+def show_winner(board):
+    cnt = {BLACK: 0, WHITE: 0, SPACE: 0}
+    for i in range(1, SIZE - 1):
+        for j in range(1, SIZE - 1):
+            cnt[board[i][j]] += 1
+
+    print('#' * 15)
+    print("黒 : {}, 白 : {}".format(cnt[BLACK], cnt[WHITE]))
+    print("{}の勝利".format("黒" if cnt[WHITE] < cnt[BLACK] else "白"))
+    print('#' * 15)
+
+
+def show_board_cui(board):
+    print()
+    print("{:1s}".format(''), end='')
+    for n in range(1, SIZE - 1):
+        print("{:3d}".format(n), end='')
+    print()
+    for i in range(1, SIZE - 1):
+        print("{:<3d}".format(i), end='')
+        for j in range(1, SIZE - 1):
+            print("{:3s}".format(BOARD[board[i][j]]), end='')
+        print()
+    print()
 
 
 def main():
@@ -135,14 +170,21 @@ def main():
     init_board(board)
 
     while True:
-        show_board(board)
+        show_board_cui(board)
+        if not exit_legal_move(board, player):
+            print(PLAYER[player] + "石を置ける場所がありません. パスします")
+            player = opp(player)
+            if not exit_legal_move(board, player):
+                print(PLAYER[player] + "石を置ける場所がありません. ゲームを終了します")
+                show_winner(board)
+                break
+
         i, j = get_move(player)
         if is_legal_move(board, player, i, j):
             set_and_turn_over(board, player, i, j)
             player = opp(player)
         else:
             print("石を置けません")
-
 
 
 if __name__ == "__main__":
